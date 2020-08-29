905e33c915d9d3ef3df000749db92808ba0aff21
Ubuntu <ubuntu@ip-172-31-24-139.us-east-2.compute.internal>
2020-08-29 21:06:09 +0000

    got data from the Array using javascript

--- index.js
+++ index.js
@@ -2,33 +2,65 @@
 
 
 let myLibrary = [
-{ name: "LEs miserables",
+{ name: "Les miserables",
   author: "Victor Hugo",
   numberOfPages: 1600
 } ,  
-{ name: "LEs miserables",
+{ name: "Une saison blanche et Seche",
+  author: "André Brink",
+  numberOfPages: 1600
+} ,  
+{ name: "Père inconnu",
   author: "Victor Hugo",
   numberOfPages: 1600
 } ,  
-{ name: "LEs miserables",
-  author: "Victor Hugo",
-  numberOfPages: 1600
-} ,  
-{ name: "LEs miserables",
-  author: "Victor Hugo",
+{ name: "La Croix du Sud",
+  author: "Joseph Ngoué",
   numberOfPages: 1600
 }
+ ,  
+{ name: "Sous la cendre le feu",
+  author: "Evelyne Mpoudi Ngollé",
+  numberOfPages: 1600
+}
+   
     
 ];
 
+let cont = document.getElementById('books');
+
+
 function render() {
+ let cont = document.getElementById('books');
+ 
+ for (let i=0; i < myLibrary.length; i++) {
  let box =  document.createElement('div');
  box.className = 'col-md-4';
+ cont.appendChild(box);
  let card = document.createElement('div');
  card.className='card';
  box.appendChild(card);
- let image = document.getElementById('imgg');
- img.src ="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+ let image = document.createElement('img');
+ image.className = 'card-img-top picture';
+ image.setAttribute("src", "https://mdbootstrap.com/img/Photos/Others/images/43.jpg");
  card.appendChild(image);
- console.log(card);
+ let cardBody = document.createElement('div');
+ cardBody.className = 'card-body';
+ card.appendChild(cardBody);
+ let cardTitle = document.createElement('h4');
+ cardTitle.className = 'card-title';
+ cardTitle.appendChild(document.createTextNode(myLibrary[i].name));
+ cardBody.appendChild(cardTitle);
+ let bookDescription = document.createElement('p');
+ bookDescription.className = 'card-text';
+ bookDescription.appendChild(document.createTextNode(myLibrary[i].numberOfPages));
+ cardBody.appendChild(bookDescription);
+ let del = document.createElement('a');
+ del.className = 'btn btn-danger';
+ del.appendChild(document.createTextNode('Delete'));
+ cardBody.appendChild(del);
+ }
+ 
+ console.log(cardBody);
 };
+
